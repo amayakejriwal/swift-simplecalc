d2f5901e31bfd0cde12e912b6d4aa1c29ea604ba
amayakejriwal <amayak@uw.edu>
2022-04-07 18:56:47 -0700

    completed extra credit

--- SimpleSwift.playground/Contents.swift
+++ SimpleSwift.playground/Contents.swift
@@ -122,12 +122,61 @@ calculate("1 -2 3 -4 5 count") == 5
  
 // Implement calculate([String]) and calculate(String)
 // to use floating-point values
-/*
+
 func calculate(_ args: [String]) -> Double {
-    return -1.0
+    // Counting
+    if args[args.count - 1] == "count" {
+        return Double(args.count - 1)
+    }
+    
+    // Avg
+    if args[args.count - 1] == "avg" && args.count > 1 {
+        var sum = 0
+        for num in args {
+            sum += Int(num) ?? 0
+        }
+        return Double(sum / (args.count - 1))
+    } else if args[args.count - 1] == "avg" {
+        return 0.0
+    }
+    
+    // Factorial
+    if args[args.count - 1] == "fact" && args.count > 1 {
+        if Int(args[0])! == 0 {
+            return 1.0
+        } else {
+            var factorial = 1
+            for num in 1...Int(args[0])! {
+                factorial *= num
+            }
+            return Double(factorial)
+        }
+    } else if args[args.count - 1] == "fact" {
+        return 0.0
+    }
+    
+    // Evaluating an expression: Ex. 2 + 2
+    let operand = args[1]
+    switch operand {
+    case "+":
+        return Double(args[0])! + Double(args[2])!
+    case "-":
+        return Double(args[0])! - Double(args[2])!
+    case "*":
+        return Double(args[0])! * Double(args[2])!
+    case "/":
+        return Double(args[0])! / Double(args[2])!
+    case "%":
+        return Double(args[0])!.truncatingRemainder(dividingBy: Double(args[2])!)
+
+    default:
+        return -1.0
+    }
 }
+
 func calculate(_ arg: String) -> Double {
-    return -1.0
+    let values = arg.split(separator: " ").map(String.init)
+    return calculate(values)
 }
 
 calculate(["2.0", "+", "2.0"]) == 4.0
@@ -136,5 +185,5 @@ calculate(["12.0", "-", "12.0"]) == 0.0
 calculate(["2.5", "*", "2.5"]) == 6.25
 calculate(["2.0", "/", "2.0"]) == 1.0
 calculate(["2.0", "%", "2.0"]) == 0.0
-calculate("1.0 2.0 3.0 4.0 5.0 count") == 5
-*/
+calculate("1.0 2.0 3.0 4.0 5.0 count") == 5.0
+
